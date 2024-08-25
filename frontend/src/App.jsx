426c1077f60aa67cb12a165e0d718bba9817def9
Vedant-314 <vedanttaak7@gmail.com>
2024-08-25 16:46:36 +0530

    header

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,28 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+
+import About from "./Components/About/About";
+import Footer from "./Components/Footer/Footer";
+import Header from "./Components/Header/Header";
+import Home from "./Components/Home/Home";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </>
+  );
 }
 
 export default App;
---
+++ frontend/src/Components/Header/Header.jsx
@@ -0,0 +1,75 @@
+import React, { useState } from "react";
+import { BsCart2 } from "react-icons/bs";
+import { Link } from "react-router-dom";
+import { FaBars } from "react-icons/fa6";
+import { RxCross2 } from "react-icons/rx";
+
+import "./header.css";
+
+function Header() {
+  const [clicked, setClicked] = useState(false);
+  const handleClick = () => setClicked(!clicked);
+  return (
+      <>
+        <div className="header-container">
+          <div className="hamburger">
+            {clicked ? (
+              <RxCross2 onClick={handleClick} />
+            ) : (
+              <FaBars onClick={handleClick} />
+            )}
+          </div>
+          <div className="left">
+            <h2>Sant Designers</h2>
+          </div>
+          <div className="middle">
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/idols">Idols</Link>
+              </li>
+              <li>
+                <Link to="/giftItems">Gifting Items</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+              <li>
+                <a href="#contactUs">Contact Us</a>
+              </li>
+            </ul>
+          </div>
+          <div className="right">
+            <span className="cart-icon">
+              <BsCart2 />
+            </span>
+          </div>
+        </div>
+        {clicked && (
+          <div className="responsive-nav">
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/idols">Idols</Link>
+              </li>
+              <li>
+                <Link to="/giftItems">Gifting Items</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+              <li>
+                <a href="#contactUs">Contact Us</a>
+              </li>
+            </ul>
+          </div>
+        )}
+      </>
+  );
+}
+
+export default Header;
