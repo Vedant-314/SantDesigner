75be889fd79186ce63cf00cc03c7a95b5de5aacc
Vedant-314 <vedanttaak7@gmail.com>
2024-09-07 22:57:10 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -4,15 +4,25 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 require('dotenv').config();
 
+const productRoutes = require('./routes/products')
+
 const PORT = process.env.PORT || process.env.API_PORT;
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.use('/api/products', productRoutes);
+
 const server = http.createServer(app);
 
+mongoose.connect(process.env.MONGO_URI).then(() =>{
+    server.listen(PORT, () =>{
+        console.log(`Server is running on port ${PORT}`);
+    }); 
+})
 
-server.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+.catch(err =>{
+    console.log('Connection Failed');
+    console.log(err);
 })
--- frontend/src/Components/Header/Header.jsx
+++ frontend/src/Components/Header/Header.jsx
@@ -5,70 +5,74 @@ import { FaBars } from "react-icons/fa6";
 import { RxCross2 } from "react-icons/rx";
 
 import "./header.css";
+import Cart from "../Cart/Cart";
 
 function Header() {
+  const [showCart, setShowCart] = useState(false);
   const [clicked, setClicked] = useState(false);
   const handleClick = () => setClicked(!clicked);
+
   return (
-      <>
-        <div className="header-container">
-          <div className="hamburger">
-            {clicked ? (
-              <RxCross2 onClick={handleClick} />
-            ) : (
-              <FaBars onClick={handleClick} />
-            )}
-          </div>
-          <div className="left">
-            <h2>Sant Designers</h2>
-          </div>
-          <div className="middle">
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/idols">Idols</Link>
-              </li>
-              <li>
-                <Link to="/giftItems">Gifting Items</Link>
-              </li>
-              <li>
-                <Link to="/about">About</Link>
-              </li>
-              <li>
-                <a href="#contactUs">Contact Us</a>
-              </li>
-            </ul>
-          </div>
-          <div className="right">
-            <span className="cart-icon">
-              <BsCart2 />
-            </span>
-          </div>
+    <>
+      <div className="header-container">
+        <div className="hamburger">
+          {clicked ? (
+            <RxCross2 onClick={handleClick} />
+          ) : (
+            <FaBars onClick={handleClick} />
+          )}
         </div>
-        {clicked && (
-          <div className="responsive-nav">
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/idols">Idols</Link>
-              </li>
-              <li>
-                <Link to="/giftItems">Gifting Items</Link>
-              </li>
-              <li>
-                <Link to="/about">About</Link>
-              </li>
-              <li>
-                <a href="#contactUs">Contact Us</a>
-              </li>
-            </ul>
-          </div>
-        )}
-      </>
+        <div className="left">
+          <h2>Sant Designers</h2>
+        </div>
+        <div className="middle">
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/idols">Idols</Link>
+            </li>
+            <li>
+              <Link to="/giftItems">Gifting Items</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <a href="#contactUs">Contact Us</a>
+            </li>
+          </ul>
+        </div>
+        <div className="right">
+          <span className="cart-icon">
+            <BsCart2 onClick={() => setShowCart(true)} />
+          </span>
+        </div>
+      </div>
+      {clicked && (
+        <div className="responsive-nav">
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/idols">Idols</Link>
+            </li>
+            <li>
+              <Link to="/giftItems">Gifting Items</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <a href="#contactUs">Contact Us</a>
+            </li>
+          </ul>
+        </div>
+      )}
+      {showCart && <Cart setShowCart={setShowCart} />}
+    </>
   );
 }
 
